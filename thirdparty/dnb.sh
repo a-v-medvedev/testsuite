38551195c5d273b9eea29628296273c2d032b2d9
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2021-03-24 18:08:09 +0300

    dnb.sh update.

--- thirdparty/dnb.sh
+++ thirdparty/dnb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MODULE=teststub
+CONF=mars
+
 set -eu
 
 [ -f ../env.sh ] && source ../env.sh || echo "WARNING: no environment file ../env.sh!"
@@ -25,10 +28,10 @@ function dnb_massivetests() {
     fi
     local COMMANDS=""
     local PARAMS="THIRDPARTY=.."
-    PARAMS="$PARAMS MODULE=imb_async"
+    PARAMS="$PARAMS MODULE=$MODULE"
     b_make "$pkg" "$V" "$COMMANDS" "clean" "$m"
     b_make "$pkg" "$V" "$COMMANDS" "$PARAMS" "$m"
-    local FILES="massivetest scripts/competing/clean.sh scripts/competing/compare.sh scripts/competing/competing_massive_tests.sh scripts/competing/make_table.sh scripts/massive_tests.inc scripts/competing/script-postproc.sh confs/imb_async_mars/modeset.inc confs/imb_async_mars/params.inc"
+    local FILES="massivetest scripts/competing/clean.sh scripts/competing/compare.sh scripts/competing/competing_massive_tests.sh scripts/competing/make_table.sh scripts/massive_tests.inc scripts/competing/script-postproc.sh confs/${MODULE}_${CONF}/modeset.inc confs/${MODULE}_${CONF}/params.inc"
     this_mode_is_set "i" "$m" && i_direct_copy "$pkg" "$V" "$FILES" "$m"
     i_make_binary_symlink "$pkg" "${V}" "$m"
     return 0
@@ -47,6 +50,22 @@ function dnb_teststub() {
 	local FILES="teststub"
 	this_mode_is_set "i" "$m" && i_direct_copy "$pkg" "$V" "$FILES" "$m"
 	i_make_binary_symlink "$pkg" "${V}" "$m"
+    if this_mode_is_set "i" "$m"; then
+        cd "$pkg".bin
+        cat > psubmit.opt.TEMPLATE << 'EOM'
+QUEUE=__QUEUE__
+QUEUE_SUFFIX=__QUEUE_SUFFIX__
+NODETYPE=__NODETYPE__
+TIME_LIMIT=3
+TARGET_BIN=./teststub
+INIT_COMMANDS=__INIT_COMMANDS__
+INJOB_INIT_COMMANDS='__INJOB_INIT_COMMANDS__'
+MPIEXEC=__MPI_SCRIPT__
+BATCH=__BATCH_SCRIPT__
+EOM
+        template_to_psubmitopts .
+        cd "$INSTALL_DIR"
+    fi
 	return 0
 }
 
@@ -83,7 +102,7 @@ function dnb_mpi-benchmarks() {
 QUEUE=__QUEUE__
 QUEUE_SUFFIX=__QUEUE_SUFFIX__
 NODETYPE=__NODETYPE__
-TIME_LIMIT=1000
+TIME_LIMIT=3
 TARGET_BIN=./IMB-ASYNC
 INIT_COMMANDS=__INIT_COMMANDS__
 INJOB_INIT_COMMANDS='__INJOB_INIT_COMMANDS__'
@@ -92,6 +111,7 @@ BATCH=__BATCH_SCRIPT__
 EOM
         template_to_psubmitopts .
         cd "$INSTALL_DIR"
+        [ -e "imb_async.bin" ] || ln -s "$pkg".bin imb_async.bin
 fi
 
 }
@@ -102,16 +122,14 @@ function dnb_sandbox() {
     mkdir -p sandbox
     [ -e sandbox/psubmit.bin ] || ln -s ../psubmit.bin sandbox/
     cp -vr massivetests.bin/* sandbox/
-	cp -vr teststub.bin/* sandbox/
-    cp -vr mpi-benchmarks.bin/* sandbox/
-    cp -vr ../teststub.conf/* sandbox/
+	cp -vr ${MODULE}.bin/* sandbox/
+    cp -vr ../${MODULE}.conf/* sandbox/
     cd sandbox
-    #template_to_psubmitopts .
     for i in always never rand1 rand2 rand5 rand10 rand50 rand90 rand95 rand99; do 
         [ -e psubmit_${i}.opt ] || ln -s psubmit.opt psubmit_${i}.opt
     done
-    ln -s .. thirdparty
-    ln -s ../../env.sh env.sh
+    [ -e thirdparty ] || ln -s .. thirdparty
+    [ -e env.sh ] || ln -s ../../env.sh env.sh
     cd ..
     echo ">> Sandbox ready"
     return 0
