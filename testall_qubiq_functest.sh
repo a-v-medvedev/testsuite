76e814f581cdf39e64ffe3a5c7c552b28904bf76
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2021-06-24 20:56:22 +0300

    qubiq tests: main cycle improved.

--- testall_qubiq_functest.sh
+++ testall_qubiq_functest.sh
@@ -2,8 +2,20 @@
 
 source thirdparty/dbscripts/base.inc
 
+RESULT=""
+
+function report() {
+    RESULT="$1"
+    return 1
+}
+
 function do_build_and_test() {
-    ./build.sh  "$url" "$app" "$testdriver" "$1" || fatal "./build.sh failed"
+    echo RUN: ./build.sh  "$url" "$app" "$testdriver" "$1"
+    echo ">> ..."
+    local t1=$(date +%s)
+    ./build.sh  "$url" "$app" "$testdriver" "$1" > build_$1.log 2>&1 || report "build_failed" || return 1
+    local t2=$(date +%s)
+    echo ">> done in $(expr $t2 - $t1) sec."
     if [ -e sandbox_$1 ]; then
         cp -ru thirdparty/sandbox/* sandbox_$1/
     else
@@ -15,8 +27,13 @@ function do_build_and_test() {
     rm psubmit.bin
     ln -s ../thirdparty/psubmit.bin .
     export LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-    ./functional_massive_tests.sh || fatal "./functional_massive_tests.sh failed"
+    echo RUN: ./functional_massive_tests.sh in sandbox_$1 directory
+    local t3=$(date +%s)
+    ./functional_massive_tests.sh > test_routine_$1.log 2>&1 || report "test_routine_failed" || return 1
+    local t4=$(date +%s)
     cd ..
+    echo ">> done in $(expr $t4 - $t3) sec."
+    report "$(expr $t2 - $t1) sec / $(expr $t4 - $t3) sec" || return 0
 }
 
 
@@ -31,33 +48,68 @@ url=${1:-${default_url}}
 export TESTSUITE_MODULE="$testdriver"
 export TESTSUITE_PROJECT="$app"
 export TESTSUITE_SCRIPT="functional"
-export TESTSUITE_BRANCH=${2:${default_branch}}
-export TESTSUITE_CONF=${3:${default_conf}}
+export TESTSUITE_BRANCH=${2:-${default_branch}}
+export TESTSUITE_CONF=${3:-${default_conf}}
 
-echo "============"
-echo STARTED AT: $(date)
-echo "============"
+echo "APP: $app"
+echo "TESTDRIVER: $testdriver"
+echo "URL: $url"
+echo "SCRIPT: $TESTSUITE_SCRIPT"
+echo "BRANCH: $TESTSUITE_BRANCH"
+echo "CONF: $TESTSUITE_CONF"
+echo "STARTED_AT: $(date)"
+
+timestamp=$(date +%s)
 
 for suite in basic; do
+    echo "SUITE_START: $suite"
     do_build_and_test $suite
+    if [ "$?" == 1 ]; then
+        case $RESULT in
+            build_failed) 
+                mv build_$suite.log log_$suite.log
+                echo "build" > timing_$suite.log
+                ;;
+            test_routine_failed) 
+                mv test_routine_$suite.log log_$suite.log
+                echo "test_routine" > timing_$suite.log
+                ;;
+            *) fatal "Unknown state after 'do_build_and_test $suite' execution."
+        esac
+        echo "STATUS: ${suite}: failrure on stage: $(cat timing_$suite.log)"
+        cat log_$suite.log
+        SUMMARY=summary_${suite}_$(cat timing_$suite.log)_failure_${timestamp}.tar.gz
+        tar czf "$SUMMARY" log_$suite.log
+    else
+        echo "$RESULT" > timing_$suite.log
+        echo "STATUS: ${suite}: actions complete."
+    fi
+    echo "SUITE_END: $suite"
 done
 
-echo "============"
-echo ENDED AT: $(date)
-echo "============"
-timestamp=$(date +%s)
+echo "ENDED_AT: $(date)"
+
 for i in sandbox_*; do
     suite=$(echo $i | sed 's/sandbox_//')
     nfailed=$(wc -l < $i/summary/references.txt)
+    echo
     echo "----------------------------------------"
-    echo "--- ${suite}: $nfailed failure references"
-    echo "----------------------------------------"
-    for j in $i/summary/table.*; do
+    if grep -q ' sec' timing_$suite.log; then
+        echo "--- ${suite}: $(cat $i/summary/stats.txt)"
+        echo "--- ${suite}: recoreded $nfailed failure references"
+        echo "--- ${suite}: processing time: $(cat timing_$suite.log)"
         echo "----------------------------------------"
-        echo "--> " $(basename $j)
-        echo "----------------------------------------"
-        cat $j
-        echo "----------------------------------------"
-    done
-    tar czf summary_${suite}_${nfailed}F_${timestamp}.tar.gz $i/summary/*
+        for j in $i/summary/table.*; do
+            echo "----------------------------------------"
+            echo "--> " $(basename $j)
+            echo "----------------------------------------"
+            cat $j
+            echo "----------------------------------------"
+        done
+        SUMMARY=summary_${suite}_${nfailed}F_${timestamp}.tar.gz
+        tar czf "$SUMMARY" $i/summary/*
+    else
+        echo "--- ${suite}: failrure on stage: $(cat timing_$suite.log)"
+    fi
 done
+
