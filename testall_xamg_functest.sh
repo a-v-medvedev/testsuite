e2196050fb8dc175d7f9a29130ac07d39820f7bc
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2021-07-01 13:53:48 +0300

    Scripts for qubiq and xamg projects updated.

--- testall_xamg_functest.sh
+++ testall_xamg_functest.sh
@@ -2,7 +2,10 @@
 
 source thirdparty/dbscripts/base.inc
 
-default_url="https://github.com/a-v-medvedev/testsuite_confs.git"
+[ -z "$DNB_GITLAB_USERNAME" ] && echo "DNB_GITLAB_USERNAME environment variable must be defined" && exit 1
+[ -z "$DNB_GITLAB_ACCESS_TOKEN" ] && echo "DNB_GITLAB_ACCESS_TOKEN environment variable must be defined" && exit 1
+
+default_url="https://$DNB_GITLAB_USERNAME:$DNB_GITLAB_ACCESS_TOKEN@gitlab.com/krasnopolsky_msu/xamg-testsuite-conf.git"
 default_branch="master"
 default_conf="generic"
 
@@ -14,7 +17,8 @@ export TESTSUITE_MODULE="$testdriver"
 export TESTSUITE_PROJECT="$app"
 export TESTSUITE_SCRIPT="functional"
 export TESTSUITE_BRANCH=${2:-${default_branch}}
-export TESTSUITE_CONF_URL=${3:-${default_conf}}
+export TESTSUITE_CONF_URL=${1:-${default_url}}
 export TESTSUITE_SUITES="blas_small spmv_small solve_basic_small"
+export TESTSUITE_BUILD_CONF=${3:-${default_conf}}
 
 source ./testall.sh
