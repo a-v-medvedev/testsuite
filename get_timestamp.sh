bf63fd2ddd569d7fa9c10d664dae7f8fb865d4ec
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2024-02-14 12:22:00 +0100

    Timestamp handling changed.

---
+++ get_timestamp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function get_timestamp() {
+    secs=$(date +%s)
+    day=$(date "+%Y.%m.%d" --date="@$secs")
+    rest=$(expr "$secs" % 86400)
+    code=$(awk -v N=$rest 'END { x1=(N/25/25/25); x2=(N/25/25)%25; x3=(N/25)%25; x4=N%25; printf "%c%c%c%c\n", 65+x1, 65+x2, 65+x3, 65+x4 }' < /dev/null)
+    echo $day.$code
+}
+
+echo "TIMESTAMP: $(get_timestamp)"
--- testall.sh
+++ testall.sh
@@ -28,13 +28,13 @@ function report() {
     return 1
 }
 
-function get_timestamp() {
-    secs=$(date +%s)
-    day=$(date "+%Y.%m.%d" --date="@$secs")
-    rest=$(expr "$secs" % 86400)
-    code=$(awk -v N=$rest 'END { x1=(N/25/25/25); x2=(N/25/25)%25; x3=(N/25)%25; x4=N%25; printf "%c%c%c%c\n", 65+x1, 65+x2, 65+x3, 65+x4 }' < /dev/null)
-    echo $day.$code
-}
+#function get_timestamp() {
+#    secs=$(date +%s)
+#    day=$(date "+%Y.%m.%d" --date="@$secs")
+#    rest=$(expr "$secs" % 86400)
+#    code=$(awk -v N=$rest 'END { x1=(N/25/25/25); x2=(N/25/25)%25; x3=(N/25)%25; x4=N%25; printf "%c%c%c%c\n", 65+x1, 65+x2, 65+x3, 65+x4 }' < /dev/null)
+#    echo $day.$code
+#}
 
 function do_build_and_test() {
     local suite="$1"
@@ -82,8 +82,8 @@ echo "SCRIPT: $TESTSUITE_SCRIPT"
 echo "CONF_URL: $TESTSUITE_CONF_URL"
 echo "STARTED_AT: $(date)"
 
-timestamp=$(get_timestamp)
-
+timestamp="$TESTSUITE_TIMESTAMP"
+[ -z "$timestamp" ] && timestamp=$(./get_timestamp.sh)
 export TESTSUITE_TIMESTAMP="$timestamp"
 echo "TIMESTAMP: $timestamp"
 
