a68e78731b9a6fa308e523d8bb2da669d4aa9696
Alexey Medvedev <alexey.medvedev@bsc.es>
2024-02-16 18:11:49 +0200

    Fix in Slurm wrapper.

--- build-slurm-wrapper.sh
+++ build-slurm-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -z "$SLURM_JOBID" -o -z "$NODE_NODEID" ] && { ./build.sh $*; exit $?; }
+[ -z "$SLURM_JOBID" -o -z "$SLURM_NODEID" ] && { echo "FATAL: this wrapper $(basename $0) is designed to be run inside a compute node."; exit 1; }
 
 if [ ! -z "$SLURM_NODEID" -a "$SLURM_LOCALID" == "0" ]; then
     ./build.sh $*
